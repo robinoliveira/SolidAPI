721030498239da27ecec1d94d921a9f9676021a3
Robin Oliveira <robin.oliveira@UPRA.local>
2022-06-29 13:10:59 +0100

    added await to fix send email

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -19,7 +19,7 @@ export class CreateUserUseCase {
     }
     const user = new User(data);
     await this.usersRepository.save(user);
-    this.mailProvider.sendMail({
+    await this.mailProvider.sendMail({
       to: {
         name: data.name,
         email: data.email,
